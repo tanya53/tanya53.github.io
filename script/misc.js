85a7b294291bda7f90536f33e7c00ab64c1b856c
tigger <tgendreau@centurytel.net>
2017-03-13 10:47:01 -0500

    Add files via upload

--- script/misc.js
+++ script/misc.js
@@ -5,9 +5,10 @@
     function getLocation(){
 
 
-    $.getJSON("http://ipinfo.io",function(location){
+    $.getJSON("https://ipinfo.io",function(location){
       $("#location").text("Bemidji, Minnesota");
       $("#location2").text(location.city+", " + location.region);
+      console.log("location ",location.dity,location.region);
     });
     }
 
@@ -24,23 +25,23 @@
 
       /* all the data is parsed from the openweathermap.org
          site, using the lat and long as input */
-      var str = "http://api.openweathermap.org/data/2.5/weather?lat="+ PEEPLAT + "&lon=" + PEEPLONG +"&APPID=0adedd5f2ad9aced6e02a4c438f532cc"
+      var str = "https://api.openweathermap.org/data/2.5/weather?lat="+ PEEPLAT + "&lon=" + PEEPLONG +"&APPID=0adedd5f2ad9aced6e02a4c438f532cc"
       $.getJSON(str,function(weather){
             var temp = Number(weather.main.temp);
             temp = (temp - 273.15) * 1.8 + 32;
             temp = Math.round(temp);
             $("#temp").text(temp + ' F');
             var iconval = weather.weather[0].icon;
-            $(".icon").html("<p><img id='img1' src='http://openweathermap.org/img/w/"+iconval+".png' alt = 'weather icon' height = '150' width='150'></p>");
+            $(".icon").html("<p><img id='img1' src='https://openweathermap.org/img/w/"+iconval+".png' alt = 'weather icon' height = '150' width='150'></p>");
 
-        var str = "http://api.openweathermap.org/data/2.5/weather?lat="+ latitude + "&lon=" + longitude +"&APPID=0adedd5f2ad9aced6e02a4c438f532cc";
+        var str = "https://api.openweathermap.org/data/2.5/weather?lat="+ latitude + "&lon=" + longitude +"&APPID=0adedd5f2ad9aced6e02a4c438f532cc";
         $.getJSON(str,function(weather){
               var temp2 = Number(weather.main.temp);
               temp2 = (temp2 - 273.15) * 1.8 + 32;
               temp2 = Math.round(temp2);
               $("#temp2").text(temp2 + ' F');
               var iconval = weather.weather[0].icon
-              $(".icon").html("<p><img id = 'img2' src='http://openweathermap.org/img/w/"+iconval+".png' alt = 'weather icon' height = '150' width='150'></p>");
+              $(".icon").html("<p><img id = 'img2' src='https://openweathermap.org/img/w/"+iconval+".png' alt = 'weather icon' height = '150' width='150'></p>");
               if (temp < temp2){
                 $("#visitinfo").after("<div style = 'color:red;font-size:2em;' class='col-xs-12'>Peepmobile loaded, stock your frig!!!!</div>")
               }else {
